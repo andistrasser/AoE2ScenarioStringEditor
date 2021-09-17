16d6c96743aae3172704679de9f688a5f9b9b8ed
andreas <andistrasser123@gmail.com>
2021-09-17 19:08:18 +0200

    implemented tab structure and save button

--- src/application.py
+++ src/application.py
@@ -7,6 +7,10 @@ from tkinter import ttk
 APP_TITLE = "AoE2ScenarioStringEditor"
 THEME_DEFAULT = "clam"
 THEME_WINDOWS = "vista"
+LABEL_TAB_PLAYERS = "Players"
+LABEL_TAB_MESSAGES = "Messages"
+LABEL_TAB_TRIGGERS = "Triggers"
+LABEL_SAVE = "Save"
 
 
 # application class
@@ -35,3 +39,20 @@ class App(tk.Tk):
             self.iconbitmap(default=icon)
         else:
             self.style.theme_use(THEME_DEFAULT)
+
+        self.tab_view = ttk.Notebook(self)
+
+        self.tab_players = ttk.Frame(self.tab_view)
+        self.tab_messages = ttk.Frame(self.tab_view)
+        self.tab_triggers = ttk.Frame(self.tab_view)
+        self.tab_view.add(self.tab_players, text=LABEL_TAB_PLAYERS)
+        self.tab_view.add(self.tab_messages, text=LABEL_TAB_MESSAGES)
+        self.tab_view.add(self.tab_triggers, text=LABEL_TAB_TRIGGERS)
+        self.tab_view.pack(expand=1, fill="both")
+        self.tab_view.grid(row=0, column=0, padx=10, pady=10, sticky="ewns")
+
+        self.button_save = ttk.Button(self, text=LABEL_SAVE)
+        self.button_save.grid(row=1, column=0, padx=10, pady=10, sticky="e")
+
+        self.columnconfigure(0, weight=3)
+        self.rowconfigure(0, weight=3)
