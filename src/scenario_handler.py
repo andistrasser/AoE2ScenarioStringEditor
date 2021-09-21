cce8dbeb754aa75e52e93ff7ba9ad1e389ceb132
andreas <andistrasser123@gmail.com>
2021-09-21 12:24:14 +0200

    implemented scenario loading method

--- src/scenario_handler.py
+++ src/scenario_handler.py
@@ -1,3 +1,9 @@
+from AoE2ScenarioParser.scenarios.aoe2_de_scenario import AoE2DEScenario
+
+
 class ScenarioHandler:
     def __init__(self, file_path):
         self.file_path = file_path
+
+    def load_scenario(self):
+        return AoE2DEScenario.from_file(self.file_path)
