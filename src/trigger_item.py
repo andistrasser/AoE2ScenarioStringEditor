23c01d273a95fa7be8c7d79dd4649064010ae0f5
andreas <andistrasser123@gmail.com>
2021-09-22 10:07:49 +0200

    added trigger_item object

---
+++ src/trigger_item.py
@@ -0,0 +1,13 @@
+# constants
+TYPE_OBJECTIVE_LONG = 0
+TYPE_OBJECTIVE_SHORT = 1
+TYPE_EFFECT_MESSAGE = 2
+
+
+class TriggerItem:
+    def __init__(self, item_type, name, text, trigg_index, effect_index):
+        self.item_type = item_type
+        self.name = name
+        self.text = text
+        self.trigger_index = trigg_index
+        self.effect_index = effect_index
